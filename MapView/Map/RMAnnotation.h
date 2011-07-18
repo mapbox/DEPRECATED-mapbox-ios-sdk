b1a46432c692c01638c68f71b0e8cd7d0f632173
Thomas Rasch <thomas.rasch@alpstein.de>
2011-07-18 18:20:28 +0200

    o Generic annotation class

--- MapView/Map/RMAnnotation.h
+++ MapView/Map/RMAnnotation.h
@@ -2,12 +2,49 @@
 //  RMAnnotation.h
 //  MapView
 //
-//  Created by Thomas Rasch on 18.07.11.
-//  Copyright 2011 Alpstein. All rights reserved.
+// Copyright (c) 2008-2009, Route-Me Contributors
+// All rights reserved.
 //
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #import <Foundation/Foundation.h>
 
 @interface RMAnnotation : NSObject
+{
+    // a type name for the annotation
+    NSString *annotationType;
+
+    CLLocationCoordinate2D coordinate;
+    NSString *title;
+
+    // provided for storage of arbitrary user data
+    id userInfo;
+}
+
+@property (nonatomic, retain) NSString *annotationType;
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) id userInfo;
+
+- (id)initForAnnotationType:(NSString *)anAnnotationType atCoordinate:(CLLocationCoordinate2D)aCoordinate andTitle:(NSString *)aTitle;
 
 @end
--- MapView/Map/RMAnnotation.m
+++ MapView/Map/RMAnnotation.m
@@ -2,22 +2,73 @@
 //  RMAnnotation.m
 //  MapView
 //
-//  Created by Thomas Rasch on 18.07.11.
-//  Copyright 2011 Alpstein. All rights reserved.
+// Copyright (c) 2008-2009, Route-Me Contributors
+// All rights reserved.
 //
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #import "RMAnnotation.h"
+#import "RMMapView.h"
+#import "RMMercatorToScreenProjection.h"
+
+@interface RMAnnotation ()
+
+@property (nonatomic, assign) RMMapView *mapView;
+
+/// expressed in projected meters. The anchorPoint of the image is plotted here.
+@property (nonatomic, assign) CGPoint position;
+
+@end
+
+#pragma mark -
 
 @implementation RMAnnotation
 
-- (id)init
+@synthesize annotationType;
+@synthesize coordinate;
+@synthesize title;
+@synthesize userInfo;
+
+@synthesize mapView;
+@synthesize position;
+
+- (id)initForAnnotationType:(NSString *)anAnnotationType atCoordinate:(CLLocationCoordinate2D)aCoordinate andTitle:(NSString *)aTitle
 {
-    self = [super init];
-    if (self) {
-        // Initialization code here.
-    }
-    
+    if (!(self = [super init]))
+        return nil;
+
+    self.annotationType = annotationType;
+    self.coordinate     = aCoordinate;
+    self.title          = aTitle;
+    self.userInfo       = nil;
+
     return self;
+}
+
+- (void)setCoordinate:(CLLocationCoordinate2D)aCoordinate
+{
+    coordinate = aCoordinate;
+
+    // Callback
 }
 
 @end
