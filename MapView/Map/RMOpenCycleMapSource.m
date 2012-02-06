a11c19a1893d6ffaa7e93248aa9622a818e49478
Thomas Rasch <thomas.rasch@alpstein.de>
2012-02-06 11:50:44 +0100

    o Backported from route-me: Changed the tile URL for OpenCycleMap

--- MapView/Map/RMOpenCycleMapSource.m
+++ MapView/Map/RMOpenCycleMapSource.m
@@ -47,7 +47,7 @@
 			  @"%@ tried to retrieve tile with zoomLevel %d, outside source's defined range %f to %f", 
 			  self, tile.zoom, self.minZoom, self.maxZoom);
 
-	return [NSURL URLWithString:[NSString stringWithFormat:@"http://andy.sandbox.cloudmade.com/tiles/cycle/%d/%d/%d.png", tile.zoom, tile.x, tile.y]];
+	return [NSURL URLWithString:[NSString stringWithFormat:@"http://tile.opencyclemap.org/cycle/%d/%d/%d.png", tile.zoom, tile.x, tile.y]];
 }
 
 - (NSString *)uniqueTilecacheKey
