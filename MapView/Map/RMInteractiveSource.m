b7dead7d9b08efd5a89e4fb7702546c7a5f2f5a7
Justin R. Miller <incanus@codesorcery.net>
2012-07-23 11:10:51 -0700

    fixes #63: memory leak

--- MapView/Map/RMInteractiveSource.m
+++ MapView/Map/RMInteractiveSource.m
@@ -567,7 +567,7 @@ RMTilePoint RMInteractiveSourceNormalizedTilePointForMapView(CGPoint point, RMMa
                     if (data && [data objectForKey:keyName])
                     {
                         NSData   *jsonData   = [NSJSONSerialization dataWithJSONObject:[data objectForKey:keyName] options:0 error:nil];
-                        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+                        NSString *jsonString = [[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding] autorelease];
                         
                         return [NSDictionary dictionaryWithObjectsAndKeys:keyName,    @"keyName",
                                                                           jsonString, @"keyJSON",
