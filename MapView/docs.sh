85def2f0b6b534493a93b5ab00a471f7cd50bba5
Justin R. Miller <incanus@codesorcery.net>
2012-08-23 00:43:28 -0700

    final tweaks to docs publishing

--- MapView/docs.sh
+++ MapView/docs.sh
@@ -3,13 +3,13 @@
 VERSION=$( git describe --tags | awk -F '-' '{ print $1 }' )
 
 appledoc \
+    --output Documentation \
     --project-name "MapBox $VERSION" \
     --project-company MapBox \
     --create-html \
+    --create-docset \
     --company-id com.mapbox \
-    --output Documentation \
     --ignore build \
-    --ignore Documentation \
     --ignore FMDB \
     --ignore GRMustache \
     --ignore .c \
@@ -39,6 +39,10 @@ appledoc \
     --ignore RMTileImage.h \
     --ignore RMTileSourcesContainer.h \
     --clean-output \
-    --no-install-docset \
+    --docset-install-path /tmp \
     --index-desc ../README.markdown \
+    --docset-atom-filename docset.atom \
+    --docset-feed-url "https://raw.github.com/mapbox/mapbox-ios-sdk/gh-pages/%DOCSETATOMFILENAME" \
+    --docset-package-url "https://github.com/downloads/mapbox/mapbox-ios-sdk/%DOCSETPACKAGEFILENAME" \
+    --publish-docset \
     .
