93be894420c41faa77ed521431b024b58017562b
Justin R. Miller <incanus@codesorcery.net>
2012-09-14 16:11:30 -0700

    refs #118: fixes to user halo in compass tracking mode

--- MapView/Map/RMMapView.m
+++ MapView/Map/RMMapView.m
@@ -3027,9 +3027,15 @@
 
     self.userLocation.layer.hidden = ( ! CLLocationCoordinate2DIsValid(self.userLocation.coordinate) || self.userTrackingMode == RMUserTrackingModeFollowWithHeading);
 
+    if (_userLocationTrackingView)
+        _userLocationTrackingView.hidden = ! CLLocationCoordinate2DIsValid(self.userLocation.coordinate);
+
     _accuracyCircleAnnotation.layer.hidden = newLocation.horizontalAccuracy <= 10;
 
     _trackingHaloAnnotation.layer.hidden = ( ! CLLocationCoordinate2DIsValid(self.userLocation.coordinate) || newLocation.horizontalAccuracy > 10 || self.userTrackingMode == RMUserTrackingModeFollowWithHeading);
+
+    if (_userHaloTrackingView)
+        _userHaloTrackingView.hidden = ( ! CLLocationCoordinate2DIsValid(self.userLocation.coordinate) || newLocation.horizontalAccuracy > 10);
 
     if ( ! [_annotations containsObject:self.userLocation])
         [self addAnnotation:self.userLocation];
