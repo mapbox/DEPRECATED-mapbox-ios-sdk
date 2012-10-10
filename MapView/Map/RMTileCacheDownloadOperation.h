0af592ac654118b839728394992b57f6f128987a
Justin R. Miller <incanus@codesorcery.net>
2012-10-10 13:58:25 -0700

    guess we should technically have this

--- MapView/Map/RMTileCacheDownloadOperation.h
+++ MapView/Map/RMTileCacheDownloadOperation.h
@@ -25,6 +25,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#import <Foundation/Foundation.h>
+
 #import "RMTile.h"
 #import "RMTileSource.h"
 #import "RMTileCache.h"
