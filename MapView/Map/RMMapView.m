mapbox/DEPRECATED-mapbox-ios-sdk

commit 49a986b8b8b89c6d6c6a78932530a6fb4a2b8d45

refs #171: annotation layer hit test cleanups

MapView/Map/RMMapView.m
@@ -1536,7 +1536,7 @@
         [self deselectAnnotation:_currentAnnotation animated:( ! [hit isKindOfClass:[RMMarker class]])];
     }
 
-    if ( ! hit)
+    if ( ! [hit isKindOfClass:[CALayer class]])
     {
         [self singleTapAtPoint:[recognizer locationInView:self]];
         return;
@@ -1556,10 +1556,6 @@
     else if ([superlayer superlayer] != nil && [[superlayer superlayer] isKindOfClass:[RMMarker class]])
     {
         [self tapOnLabelForAnnotation:[((RMMarker *)[superlayer superlayer]) annotation] atPoint:[recognizer locationInView:self]];
-    }
-    else
-    {
-        [self singleTapAtPoint:[recognizer locationInView:self]];
     }
 }
 

commit d9886bd1dc45a9a26c0a79f164f05c68a20e0b97

reverting bug inadvertently introduced in 49a986b8

MapView/Map/RMMapView.m
@@ -1512,6 +1512,10 @@
     {
         [self tapOnLabelForAnnotation:[((RMMarker *)[superlayer superlayer]) annotation] atPoint:[recognizer locationInView:self]];
     }
+    else
+    {
+        [self singleTapAtPoint:[recognizer locationInView:self]];
+    }
 }
 
 - (void)doubleTapAtPoint:(CGPoint)aPoint