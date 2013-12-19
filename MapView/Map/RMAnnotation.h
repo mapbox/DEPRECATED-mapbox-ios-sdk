63a2b9913c763a905ff2f07a101b21cce3275cc0
Justin R. Miller <incanus@codesorcery.net>
2013-12-19 10:44:03 -0800

    clarify user location annotation layer customization

--- MapView/Map/RMAnnotation.h
+++ MapView/Map/RMAnnotation.h
@@ -110,7 +110,9 @@
 
 /** @name Filtering Types of Annotations */
 
-/** Whether the annotation is related to display of the user's location. Useful for filtering purposes when providing annotation layers in the delegate. */
+/** Whether the annotation is related to display of the user's location. Useful for filtering purposes when providing annotation layers in the delegate. 
+*
+*   There are three possible user location annotations, depending on current conditions: the user dot, the pulsing halo, and the accuracy circle. All may have custom layers provided, but if you only want to customize the user dot, you should check that the annotation is a member of the RMUserLocation class in order to ensure that you are altering only the correct annotation layer. */
 @property (nonatomic, readonly) BOOL isUserLocationAnnotation;
 
 #pragma mark -
