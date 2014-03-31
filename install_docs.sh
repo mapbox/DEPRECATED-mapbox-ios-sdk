f7a83aa674ff1c3b48f7c0bf3f3014ad9b509f91
Justin R. Miller <incanus@codesorcery.net>
2014-03-31 13:04:29 -0700

    add new docs scripts

---
+++ install_docs.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ -z `which appledoc` ]; then
+    echo "Unable to find appledoc. Consider installing it from source or Homebrew."
+    exit 1
+fi
+
+VERSION=$( git tag | sort -r | sed -n '1p' )
+echo "Creating new docs for $VERSION..."
+echo
+
+appledoc \
+    --output /tmp/`uuidgen` \
+    --project-name "Mapbox $VERSION" \
+    --project-company Mapbox \
+    --create-docset \
+    --company-id com.mapbox \
+    --ignore build \
+    --ignore FMDB \
+    --ignore GRMustache \
+    --ignore SMCalloutView \
+    --ignore .c \
+    --ignore .m \
+    --ignore RMAttributionViewController.h \
+    --ignore RMBingSource.h \
+    --ignore RMCoordinateGridSource.h \
+    --ignore RMDBMapSource.h \
+    --ignore RMFoundation.h \
+    --ignore RMFractalTileProjection.h \
+    --ignore RMGenericMapSource.h \
+    --ignore RMGlobalConstants.h \
+    --ignore RMLoadingTileView.h \
+    --ignore RMMapOverlayView.h \
+    --ignore RMMapQuestOpenAerialSource.h \
+    --ignore RMMapQuestOSMSource.h \
+    --ignore RMMapScrollView.h \
+    --ignore RMMapTiledLayerView.h \
+    --ignore RMNotifications.h \
+    --ignore RMOpenCycleMapSource.h \
+    --ignore RMOpenSeaMapLayer.h \
+    --ignore RMOpenSeaMapSource.h \
+    --ignore RMPixel.h \
+    --ignore RMProjection.h \
+    --ignore RMTile.h \
+    --ignore RMTileImage.h \
+    --ignore RMTileSourcesContainer.h \
+    --index-desc README.markdown \
+    .
