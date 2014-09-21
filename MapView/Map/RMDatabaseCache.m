0f491dcd9a2929ec72fdd52d1f1f69555ad74717
mglasgow <mglasgow@users.noreply.github.com>
2014-09-20 20:48:39 -0600

    perf improvement to RMDatabaseCache when counting existing tiles

--- MapView/Map/RMDatabaseCache.m
+++ MapView/Map/RMDatabaseCache.m
@@ -308,7 +308,7 @@
 
     [_queue inDatabase:^(FMDatabase *db)
      {
-         FMResultSet *results = [db executeQuery:@"SELECT COUNT(tile_hash) FROM ZCACHE"];
+         FMResultSet *results = [db executeQuery:@"SELECT COUNT(*) FROM ZCACHE"];
 
          if ([results next])
              count = [results intForColumnIndex:0];
