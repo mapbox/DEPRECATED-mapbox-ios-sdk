0e1ecc3d76501e998e5dac0e2fd7b9a0c41633cc
Justin R. Miller <incanus@codesorcery.net>
2015-01-28 16:50:05 -0800

    add hosted docs script & update release procedure

--- RELEASING.md
+++ RELEASING.md
@@ -1,5 +1,7 @@
 # Releasing
 
+## GitHub
+
 1. Choose a version number per [Semantic Versioning](http://semver.org/). Let's call it `x.y.z`. 
 1. If appropriate, update `screenshot.png` in the `packaging` branch. 
 1. Switch to `release` at a point where the release code is ready to go. 
@@ -9,8 +11,19 @@
     - The local spec should reference the version above, not a branch. 
     - Be sure to lint the spec with `pod spec lint`. 
 1. Create a tag with `x.y.z` in `release` and push the tag. 
+
+## www.mapbox.com
+
 1. Update the website docs in the `mb-pages` branch as appropriate. Be sure to update `version` in `_config.yml` and `_config.mb-pages.yml` with `x.y.z`. These docs might also reference `screenshot.png` if it was updated above, so be sure to check context. 
+1. Copy the `update_docs.sh` script from the `packaging` branch into `release` and run it to update `./api` with HTML documentation output. 
+1. Copy `./api` over to `mb-pages` for publishing on the site under `/api`. 
+
+## CocoaPods
+
 1. Release on CocoaPods via `pod trunk push`. 
+
+## Binary download
+
 1. Switch to `packaging` and copy aside the `package.sh` script. 
 1. Switch back to `release`, copy in `package.sh`, and run it. 
 1. Zip up the product in `./dist` in a folder named `Mapbox-iOS-SDK-x.y.z` and update [S3](http://mapbox-ios-sdk.s3.amazonaws.com/index.html) with:
---
+++ update_docs.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ -z `which appledoc` ]; then
+    echo "Unable to find appledoc. Consider installing it from source or Homebrew."
+    exit 1
+fi
+
+OUTPUT="/tmp/`uuidgen`"
+
+VERSION=$( git tag | sort -r | sed -n '1p' )
+echo "Creating new docs for $VERSION..."
+echo
+
+appledoc \
+    --output $OUTPUT \
+    --project-name "Mapbox $VERSION" \
+    --project-company Mapbox \
+    --create-html \
+    --no-create-docset \
+    --no-install-docset \
+    --company-id com.mapbox \
+    --ignore build \
+    --ignore FMDB \
+    --ignore GRMustache \
+    --ignore SMCalloutView \
+    --ignore .c \
+    --ignore .m \
+    --ignore RMAttributionViewController.h \
+    --ignore RMBingSource.h \
+    --ignore RMCoordinateGridSource.h \
+    --ignore RMDBMapSource.h \
+    --ignore RMFoundation.h \
+    --ignore RMFractalTileProjection.h \
+    --ignore RMGenericMapSource.h \
+    --ignore RMGlobalConstants.h \
+    --ignore RMLoadingTileView.h \
+    --ignore RMMapOverlayView.h \
+    --ignore RMMapQuestOpenAerialSource.h \
+    --ignore RMMapQuestOSMSource.h \
+    --ignore RMMapScrollView.h \
+    --ignore RMMapTiledLayerView.h \
+    --ignore RMNotifications.h \
+    --ignore RMOpenCycleMapSource.h \
+    --ignore RMOpenSeaMapLayer.h \
+    --ignore RMOpenSeaMapSource.h \
+    --ignore RMPixel.h \
+    --ignore RMProjection.h \
+    --ignore RMTile.h \
+    --ignore RMTileImage.h \
+    --ignore RMTileSourcesContainer.h \
+    --index-desc README.markdown \
+    .
+
+rm -rf ./api
+mkdir ./api
+mv -v $OUTPUT/html/* ./api
